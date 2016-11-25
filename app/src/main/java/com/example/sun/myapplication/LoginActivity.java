8af235599f45d6ced99ce2592c301f348e1e9893
Sunbelife <Sunbelife@foxmail.com>
2016-11-25 12:31:19 +0800

    Add Score Count, a login UI (not finished yet) , and a cheat funchtion(not finished yet too).

---
+++ app/src/main/java/com/example/sun/myapplication/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.example.sun.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText mUsernameET;
+    private Button mLoginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mUsernameET = (EditText) findViewById(R.id.username_ET);
+
+        mLoginBtn = (Button) findViewById(R.id.login_Btn);
+        mLoginBtn.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login_Btn:
+                Intent i = new Intent();
+                i.putExtra("username", mUsernameET.getText());
+                startActivity(new Intent(LoginActivity.this, QuestionActivity.class));
+                break;
+            default:
+                break;
+        }
+    }
+
+}
--- app/src/main/java/com/example/sun/myapplication/QuestionActivity.java
+++ app/src/main/java/com/example/sun/myapplication/QuestionActivity.java
@@ -1,5 +1,6 @@
 package com.example.sun.myapplication;
 
+import android.content.Intent;
 import android.os.PersistableBundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
@@ -12,12 +13,15 @@ import android.widget.Toast;
 
 // 控制器
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class QuestionActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
     private Button mBackButton;
+    private Button mCheatButton;
+    private TextView mUsername;
+    private TextView mScoreTextview;
     private TextView mQuestionTextView;
     private Question[] mQuestionBank = new Question[] {
             new Question(R.string.question_oceans, true),
@@ -29,7 +33,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private int mCurrentIndex = 0;
     private int isTrue;
     private static final String KEY_INDEX = "index";
-    private static final String TAG = "MainActivity";
+    private static final String TAG = "QuestionActivity";
+    private static int score = 0;
+//    private Intent i;
 
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
@@ -38,10 +44,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mQuestionTextView.setText(question);
     }
 
+    private void updateScore() {
+        mScoreTextview.setText("得分:" + score);
+    }
+
     private int checkAnswer(Question q, boolean stat){
         if (q.isAnswerTrue() == stat) {
+            score = score + 5;
             return R.string.correct_toast;
         } else {
+            score = score - 5;
             return R.string.incorrect_toast;
         }
     }
@@ -81,7 +93,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mNextButton = (Button) findViewById(R.id.next_Btn);
         mNextButton.setOnClickListener(this);
 
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(this);
+
+        mScoreTextview = (TextView) findViewById(R.id.score_text_view);
+
+
+//        mUsername = (TextView) findViewById(R.id.user_TV);
+//        mUsername.setText(i.getBundleExtra("username").to);
+
+
+
         updateQuestion();
+        updateScore();
 
         Log.d(TAG,"onCreate Bundle Loaded");
 }
@@ -121,10 +145,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.true_Btn:
                 isTrue = checkAnswer(mQuestionBank[mCurrentIndex],true);
                 Toast.makeText(view.getContext(),isTrue,Toast.LENGTH_SHORT).show();
+                updateScore();
                 break;
             case R.id.false_Btn:
                 isTrue = checkAnswer(mQuestionBank[mCurrentIndex],false);
                 Toast.makeText(view.getContext(),isTrue,Toast.LENGTH_SHORT).show();
+                updateScore();
                 break;
             case R.id.next_Btn:
             case R.id.question_text_view:
@@ -134,6 +160,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.back_Btn:
                 mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
                 updateQuestion();
+                break;
+            case R.id.cheat_button:
+
                 break;
             default:
                 break;
