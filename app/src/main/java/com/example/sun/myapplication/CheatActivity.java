1c5a77f5012416ed8d9b6e56fd01bfa6abf51e4d
Sunbelife <Sunbelife@foxmail.com>
2016-12-02 11:50:34 +0800

    1. Save question and score data when you cheat. 2. add ReportActivity.(not finish yet) 3. adjust some code.

--- app/src/main/java/com/example/sun/myapplication/CheatActivity.java
+++ app/src/main/java/com/example/sun/myapplication/CheatActivity.java
@@ -1,5 +1,6 @@
 package com.example.sun.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,10 @@ public class CheatActivity extends AppCompatActivity implements View.OnClickList
     private Button mShowAnswers;
     private TextView mAnswer_TV;
     private Button mBackToQues;
+    private static final String EXTRA_ANSWER_IS_TRUE =
+            "com.example.sun.myapplication.answer_is_true";
+    private static final String EXTRA_ANSWER_SHOWN =
+            "com.example.sun.myapplication.answer_shown";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +35,7 @@ public class CheatActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.showAnswerBtn:
-                mAnswer = getIntent().getBooleanExtra("answeristrue",false);
+                mAnswer = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false);
                 if (mAnswer) {
                     mAnswer_TV.setText(R.string.strue_Btn);
                 } else {
@@ -38,11 +43,31 @@ public class CheatActivity extends AppCompatActivity implements View.OnClickList
                 }
                 break;
             case R.id.backtoQues_Btn:
-                Intent backtoQuest = new Intent(v.getContext(),QuestionActivity.class);
-                startActivity(backtoQuest);
+                finish();
                 break;
             default:
                 break;
         }
+        setAnswerShownResult(true);
+    }
+
+    // 这个方法实现了在这个类中声明绑定到这一 Activity 的 Intent，这样在别的 Activity 里只需要调用这个方法
+    // 就可以得到这个 Intent,而且在下面的代码中，Extra 的标识符也不用在其他地方搞了，而且修改起来也很简单。
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue)  {
+        Intent i = new Intent(packageContext, CheatActivity.class);
+        i.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        return i;
+    }
+
+
+    public static boolean wasAnswerShown(Intent result) {
+        return result.getBooleanExtra(EXTRA_ANSWER_SHOWN, false);
+    }
+
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
     }
 }
--- app/src/main/java/com/example/sun/myapplication/QuestionActivity.java
+++ app/src/main/java/com/example/sun/myapplication/QuestionActivity.java
@@ -1,5 +1,6 @@
 package com.example.sun.myapplication;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
@@ -9,20 +10,47 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 // 控制器
 
 public class QuestionActivity extends AppCompatActivity implements View.OnClickListener {
 
+    // 成员初始化
+    // 布局部分
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
     private Button mBackButton;
-    private Button mCheatButton;
     private TextView mScoreTextview;
     private TextView mQuestionTextView;
     private TextView mUsernameTextview;
+    private TextView mCheatCountTextView;
+    private TextView mIsaCheaterTextView;
+
+    // 数据部分
+    private int mCurrentIndex = 0;
+
+    // Intent Tag
+    private static final String KEY_INDEX = "index";
+    private static final String USER_NAME = "username";
+    private static final String SCORE = "score";
+    private static final String QUESTION_NUM = "questionnum";
+
+    // Log Tag
+    private static final String TAG = "QuestionActivity";
+    private int mScore = 0;
+    private boolean isTrue = false;
+    private int mCheatnum = 0; // 作弊次数
+    private int mNumcount = 1; // 答题数量
+    private String mUsername = "无名氏";
+
+    // 检测作弊
+    private static final int REQUEST_CODE_CHEAT = 0;
+    private boolean mIsCheater;
+
+    // 题目初始化
     private Question[] mQuestionBank = new Question[] {
             new Question(R.string.question_oceans, true),
             new Question(R.string.question_mideast, false),
@@ -30,25 +58,12 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
             new Question(R.string.question_americas, true),
             new Question(R.string.question_asia, true),
     };
-    private int mCurrentIndex = 0;
-    private static final String KEY_INDEX = "index";
-    private static final String USER_NAME = "username";
-    private static final String SCORE = "score";
-    private static final String QUESTION_NUM = "questionnum";
-    private static final String TAG = "QuestionActivity";
-    private int mScore = 0;
-    private boolean isTrue = false;
-    private int mNumcount = 1;
-    private String mUsername = "无名氏";
 
-    private void updateQuestion() {
-        int question = mQuestionBank[mCurrentIndex].getTextResId();
-        // Log.d(TAG, "Updating question text for question #" + mCurrentIndex,
-//                new Exception());
-        mQuestionTextView.setText(question);
+    private void setQuestion(int mQuestion) {
+        mQuestionTextView.setText(mQuestionBank[mQuestion].getTextResId());
     }
 
-    private void updateScore() {
+    private void checkScore(int mScore) {
         if(mScore < 0) {
             new AlertDialog.Builder(QuestionActivity.this)
                     .setTitle("你输啦")
@@ -56,12 +71,19 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
                     .setPositiveButton("重新开始", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
-                            Intent z = new Intent(QuestionActivity.this,LoginActivity.class);
+                            Intent z = new Intent(QuestionActivity.this, LoginActivity.class);
                             startActivity(z);
                         }
                     }).show();
         } else {
             mScoreTextview.setText("得分:" + mScore);
+        }
+    }
+    private void checkQuestionNum(int mQuestionnum) {
+        if (mQuestionnum == 0) {
+            mBackButton.setVisibility(View.INVISIBLE);
+        } else {
+            mBackButton.setVisibility(View.VISIBLE);
         }
     }
 
@@ -96,32 +118,9 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-        mQuestionTextView.setOnClickListener(this);
-
-        mTrueButton = (Button) findViewById(R.id.true_Btn);
-        mTrueButton.setOnClickListener(this);
-        mFalseButton = (Button) findViewById(R.id.false_Btn);
-        mFalseButton.setOnClickListener(this);
-
-        mBackButton = (Button) findViewById(R.id.back_Btn);
-        mBackButton.setOnClickListener(this);
-        mNextButton = (Button) findViewById(R.id.next_Btn);
-        mNextButton.setOnClickListener(this);
-
-//        mCheatButton = (Button) findViewById(R.id.cheat_button);
-//        mCheatButton.setOnClickListener(this);
-
-        mScoreTextview = (TextView) findViewById(R.id.score_Tv);
-
-        mUsernameTextview = (TextView) findViewById(R.id.user_TV);
-        mUsername = getIntent().getStringExtra(USER_NAME);
-        mUsernameTextview.setText("哈喽，" + mUsername + "，这是你的第" + mNumcount + "题");
-//        mUsername = (TextView) findViewById(R.id.user_TV);
-//        mUsername.setText(i.getBundleExtra("username").to);
+        init(); // 初始化组件
 
         // 还原 InstanceState
-
         if (savedInstanceState != null) {
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX);
             mUsername = savedInstanceState.getString(USER_NAME);
@@ -130,37 +129,40 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
             mUsernameTextview.setText("哈喽，" + mUsername + "，这是你的第" + mNumcount + "题");
         }
 
-        // 初始化题目和分数
-
-        updateScore();
-        updateQuestion();
 }
     public void onClick(View view) {
+        checkCheater(mIsCheater,mCheatnum);
         switch (view.getId()) {
             case R.id.true_Btn:
                 isTrue = checkAnswer(mQuestionBank[mCurrentIndex],true);
                 ShowResult(isTrue,view);
-                updateScore();
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                setQuestion((mCurrentIndex) % mQuestionBank.length);
                 break;
             case R.id.false_Btn:
                 isTrue = checkAnswer(mQuestionBank[mCurrentIndex],false);
                 ShowResult(isTrue,view);
-                updateScore();
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                setQuestion((mCurrentIndex) % mQuestionBank.length);
                 break;
             case R.id.next_Btn:
-                mNumcount++;
                 mUsernameTextview.setText("哈喽，" + mUsername + "，这是你的第" + mNumcount + "题");
             case R.id.question_text_view:
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                updateQuestion();
+                setQuestion((mCurrentIndex) % mQuestionBank.length);
                 break;
             case R.id.back_Btn:
                 mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
-                updateQuestion();
+                setQuestion((mCurrentIndex) % mQuestionBank.length);
                 break;
             default:
                 break;
         }
+        mNumcount++;
+        mUsernameTextview.setText("哈喽，" + mUsername + "，这是你的第" + mNumcount + "题");
+        checkScore(mScore);
+        checkQuestionNum(mCurrentIndex);
+        Log.d(TAG,mCurrentIndex+"");
     }
 
     @Override
@@ -185,6 +187,13 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
     protected void onResume() {
         super.onResume();
         Log.d(TAG,"onResume");
+
+        // 恢复分数和题目
+
+        Log.d(TAG,"mCurrentIndex:" + mCurrentIndex);
+
+        checkScore(mScore);
+        setQuestion(mCurrentIndex);
     }
 
     @Override
@@ -192,8 +201,64 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
         super.onRestart();
         Log.d(TAG,"onRestart");
     }
-    private void ShowResult(boolean istrue, View view) {
-        if(istrue) {
+
+
+    // 处理 CheatActivity 的返回值
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) { // 如果未返回正确，直接返回。
+            return;
+        }
+        if (requestCode == REQUEST_CODE_CHEAT) { // 判断返回状态
+            if (data == null) {
+                return;
+            }
+            mIsCheater = CheatActivity.wasAnswerShown(data); // 检测作弊状态
+            if (mIsCheater) {
+                mCheatnum++;
+            }
+        }
+    }
+
+    // 初始化组件
+    protected void init() {
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        mQuestionTextView.setText(mQuestionBank[mCurrentIndex].getTextResId());
+        mQuestionTextView.setOnClickListener(this);
+
+        mTrueButton = (Button) findViewById(R.id.true_Btn);
+        mTrueButton.setOnClickListener(this);
+        mFalseButton = (Button) findViewById(R.id.false_Btn);
+        mFalseButton.setOnClickListener(this);
+
+        mBackButton = (Button) findViewById(R.id.back_Btn);
+        mBackButton.setVisibility(View.INVISIBLE);
+        mBackButton.setOnClickListener(this);
+        mNextButton = (Button) findViewById(R.id.next_Btn);
+        mNextButton.setOnClickListener(this);
+
+//        mCheatButton = (Button) findViewById(R.id.cheat_button);
+//        mCheatButton.setOnClickListener(this);
+
+        mScoreTextview = (TextView) findViewById(R.id.score_Tv);
+        mScoreTextview.setText("得分: 0");
+
+        mUsernameTextview = (TextView) findViewById(R.id.user_TV);
+        mUsername = getIntent().getStringExtra(USER_NAME);
+        mUsernameTextview.setText("哈喽，" + mUsername + "，这是你的第" + mNumcount + "题");
+
+        mCheatCountTextView = (TextView) findViewById(R.id.cheat_Tv);
+
+        mCheatCountTextView.setText("您的作弊次数:" + mCheatnum);
+
+        mIsaCheaterTextView = (TextView) findViewById(R.id.isaCheater_Tv);
+
+//        mUsername = (TextView) findViewById(R.id.user_TV);
+//        mUsername.setText(i.getBundleExtra("username").to);
+    }
+
+    private void ShowResult(boolean answeristrue, View view) {
+        if(answeristrue) {
             new AlertDialog.Builder(view.getContext())
                     .setTitle("不错嘛你")
                     .setMessage("答对了，加5分")
@@ -207,12 +272,26 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
                     .setNegativeButton("偷看答案", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
-                            Intent z = new Intent(QuestionActivity.this, CheatActivity.class);
-                            z.putExtra("answeristrue",mQuestionBank[mCurrentIndex].isAnswerTrue());
-                            startActivity(z);
+                            Intent z = CheatActivity.newIntent(QuestionActivity.this,mQuestionBank[mCurrentIndex].isAnswerTrue());
+                            startActivityForResult(z, REQUEST_CODE_CHEAT);
                         }
                     })
                     .show();
         }
+    }
+
+    private void checkCheater(boolean isCheater, int mCheatCount) {
+        if (!isCheater) {
+            mIsaCheaterTextView.setVisibility(View.INVISIBLE);
+            mCheatCountTextView.setVisibility(View.INVISIBLE);
+            return;
+        } else {
+            mIsaCheaterTextView.setVisibility(View.VISIBLE);
+            mCheatCountTextView.setVisibility(View.VISIBLE);
+        }
+        this.mIsCheater = isCheater;
+        this.mCheatnum = mCheatCount;
+        mIsaCheaterTextView.setText(R.string.isacheater_str);
+        mCheatCountTextView.setText(mCheatnum + "");
     }
 }
